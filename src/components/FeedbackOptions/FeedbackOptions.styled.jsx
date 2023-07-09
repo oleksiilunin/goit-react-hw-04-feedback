10df4a4c503cd45fd48acbe4cb7fde649709eff9
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-09 03:33:48 +0300

    change slyles for buttons in FeedbackOptions

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -23,32 +23,39 @@ const Button = styled.button`
   padding: 8px;
   width: 100%;
   border-radius: 8px;
-  border: 1px solid #cececf;
+  border: 1px solid transparent;
 
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
   background-color: #d2d8db;
   color: #666666;
 
   font-size: 16px;
   font-weight: 600;
+  background-color: ${props => {
+    if (props['data-rating'] === 'good') {
+      return '#00ff00';
+    } else if (props['data-rating'] === 'neutral') {
+      return '#ffff00';
+    } else if (props['data-rating'] === 'bad') {
+      return '#ff0000';
+    } else {
+      return '#d2d8db';
+    }
+  }};
 
-  transition: box-shadow 0.3s ease;
+  /* transition: box-shadow 0.3s ease; */
+  box-shadow: #b4b4b4 2px 2px 0px;
+  transition: transform 200ms, box-shadow 200ms;
+
+  &:active {
+    transform: translateX(2px) translateY(2px);
+    box-shadow: #b4b4b4 0px 0px 0px;
+  }
 
   &:hover,
   &:focus {
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+    /* box-shadow: #b4b4b4 2px 2px 0px; */
     cursor: pointer;
-    background-color: ${props => {
-      if (props['data-rating'] === 'good') {
-        return '#00ff00';
-      } else if (props['data-rating'] === 'neutral') {
-        return '#ffff00';
-      } else if (props['data-rating'] === 'bad') {
-        return '#ff0000';
-      } else {
-        return '#d2d8db';
-      }
-    }};
   }
 `;
 
